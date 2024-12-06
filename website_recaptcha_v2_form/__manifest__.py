5425df37eea12d3d0481ebfcf4b3937e53294d41
edescalona <ediliodaciel@gmail.com>
2024-12-05 21:13:46 -0500

    [IMP] Adding recaptcha validation in login, password reset , signup, widget form website

--- website_recaptcha_v2_form/__manifest__.py
+++ website_recaptcha_v2_form/__manifest__.py
@@ -1,8 +1,8 @@
 {
-    "name": "Website reCAPTCHA v2 login",
+    "name": "Website reCAPTCHA v2 form",
     "version": "16.0.1.0.0",
     "category": "Website",
-    "depends": ["web", "website_recaptcha_v2"],
+    "depends": ["web", "auth_signup", "website", "website_recaptcha_v2"],
     "author": """
         Binhex,
         Odoo Community Association (OCA)
@@ -12,6 +12,17 @@
     "summary": "Module to add reCAPTCHA v2 to the login form on the website",
     "data": [
         "views/webclient_templates.xml",
+        "views/auth_signup_login_templates.xml",
+        "views/s_website_form.xml",
     ],
+    "assets": {
+        "website.assets_wysiwyg": [
+            "website_recaptcha_v2_form/static/src/xml/website_form_editor.xml",
+            "website_recaptcha_v2_form/static/src/snippets/s_website_form/options.js",
+        ],
+        "web.assets_frontend": [
+            "website_recaptcha_v2_form/static/src/css/recaptcha.css",
+        ],
+    },
     "installable": True,
 }
--- website_recaptcha_v2_form/models/website.py
+++ website_recaptcha_v2_form/models/website.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import api, models
 from odoo.exceptions import AccessDenied
 
 
@@ -14,8 +14,12 @@ class Website(models.Model):
             kw: Data sent from the form
     """
 
-    def valid_recaptcha(self, kw):
-        valid, message = self.is_recaptcha_v2_valid(kw)
+    def valid_recaptcha(self, values):
+        valid, message = self.is_recaptcha_v2_valid(values)
         if not valid:
             raise AccessDenied(message)
         return True
+
+    @api.model
+    def get_recaptcha_v2_site_key(self):
+        return self.sudo().get_current_website().recaptcha_v2_site_key
