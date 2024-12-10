34c7b4f335a2d13bbbb56e5100f05b99ef77749b
edescalona <ediliodaciel@gmail.com>
2024-12-10 10:34:02 -0500

    [FIX] website_recaptcha_v2_form: Remove comments

--- website_recaptcha_v2_form/models/website.py
+++ website_recaptcha_v2_form/models/website.py
@@ -5,15 +5,6 @@ from odoo.exceptions import AccessDenied
 class Website(models.Model):
     _inherit = "website"
 
-    # --------------------------------------------------
-    # METHODS
-    # --------------------------------------------------
-    """
-        Validating that the recaptcha sent is correct
-        @params:
-            kw: Data sent from the form
-    """
-
     def valid_recaptcha(self, values):
         valid, message = self.is_recaptcha_v2_valid(values)
         if not valid:
